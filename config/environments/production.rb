b0988ab13834858d0eb3e4c785344021e0c3cf52
Shinichi Yoshida <syin.yoshida@gmail.com>
2015-02-25 11:49:54 +0900

    update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,5 @@ Railstext::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+#sy  config.active_record.dump_schema_after_migration = false
 end
