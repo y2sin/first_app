bae16ba110edf1d219ac8508100ac94f7a5e6153
Shinichi Yoshida <syin.yoshida@gmail.com>
2015-02-25 11:33:29 +0900

    update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-FirstApp::application.configure do
+DemoApp::application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
