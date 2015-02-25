1f1e9d9ef6f9b940e42ab0ce4acb1fe8dae34932
Shinichi Yoshida <syin.yoshida@gmail.com>
2015-02-25 11:43:55 +0900

    update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Railstext::application.configure do
+Railstext::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
